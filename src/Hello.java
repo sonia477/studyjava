e1fab6e88f6467306e6570198b0b83db5fc8b04b
sonia477 <sonia47744@gmail.com>
2022-01-03 23:35:06 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,28 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl + shift + L 단축키 전체 목록 보기
+		// ctrl + '+','-'
+		// ctrl + D 한 줄 삭제
+		System.out.println("/* 주석 아님 Hello, 123 world.*/");
+		System.out.println("Hello, 123 world.");
+		// ctrl + alt + down 이행 단위 복사
+		// alt + shift + A 멀이티컬럼 편집
+		// alt + up, down 행 단위 이동
+		// 초보서영
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석처리 toggle key -> 한줄 주석
+		/* 여러 줄 주석 시작
+		System.out.println("Hello, 123 world.");
+		System.out.println("Hello, 123 world.");
+		끝 -> */
+		// ctrl + space 자동완성 (ex. sysout 입력 후 ctrl + space)
+		// window>preferences>java>editor>template
+		System.out.println();
+		System.out.println("Hello, 123 world.");
+		System.out.println("Hello, 123 world.");
+	}
+
+}
