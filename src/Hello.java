2b13e20987e19e7941d03eb7d71f48012c505035
sonia477 <sonia47744@gmail.com>
2022-01-04 01:06:44 +0900

    final commit

--- src/Hello.java
+++ src/Hello.java
@@ -20,6 +20,7 @@ public class Hello {
 		�� -> */
 		// ctrl + space �ڵ��ϼ� (ex. sysout �Է� �� ctrl + space)
 		// window>preferences>java>editor>template
+		// change applied
 		System.out.println();
 		System.out.println("Hello, 123 world.");
 		System.out.println("Hello, 123 world.");
