d9bced07c71ad79c9e73e47ae9255002440b7f04
sonia477 <96939332+sonia477@users.noreply.github.com>
2022-01-04 14:20:09 +0900

    Create Ex2_1.java

---
+++ Ex2_1.java
@@ -0,0 +1,16 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// ctrl + alt + down -> copy line
+		// alt + shift + a -> column edit mode(toggle)
+		// print() -> 占쏙옙占� 占쏙옙 占쌕바뀐옙 占쏙옙占쏙옙
+		// println() -> 占쏙옙占� 占쏙옙 占쌕바뀐옙 占쏙옙
+		System.out.println(5+3); //占쏙옙占쏙옙
+		System.out.println(5-3); //占쏙옙占쏙옙
+		System.out.println(5*3); //占쏙옙占쏙옙
+		System.out.println(5/3.0); //占쏙옙占쏙옙占쏙옙 (1.666)
+		System.out.println(5/3); //占쏙옙占쏙옙占쏙옙 (1) 
+	}
+
+}
