5584c29e92c0906074dec313d695312f0f1c6207
sonia477 <96939332+sonia477@users.noreply.github.com>
2022-01-05 21:57:38 +0900

    Add files via upload

---
+++ VarEx4.java
@@ -0,0 +1,26 @@
+
+public class VarEx4 {
+
+	public static void main(String[] args) {
+		final int score; //= 100;
+		score = 200;
+		boolean power = false;
+		byte b = 127; // -128 ~ 127
+		
+		int oct = 010; //8진수, 10진수로 8
+		int hex = 0x10; //16진수, 10진수로 16
+		
+		long l = 1000_000_000_000L;
+		
+		float f = 3.14f;
+		double d = 3.14;
+		System.out.println(f);
+		System.out.println(d);
+		
+		System.out.println(10.);
+		System.out.println(.10);
+		System.out.println(10f);
+		System.out.println(1e3); // 1e3 = 10^3
+	}
+
+}
